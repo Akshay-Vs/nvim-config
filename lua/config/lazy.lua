c8ece9a9f70e91f32dc571c5b2d6eb56d8aa03d3
Akshay vs <me.akshay.vs@gmail.com>
2024-10-04 23:42:23 +0530

    feat: add autocompletion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
     { import = "plugins.lsp-config" },
     { import = "plugins.none-ls" },
     { import = "plugins.alpha" },
+    { import = "plugins.completions" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
